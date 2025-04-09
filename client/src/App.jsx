fe15eca74ec1cff6526bffcf3e01107fee666737
Ravindu Nishan Rathnayake <nish.ravindu@gmail.com>
2025-04-09 12:48:20 +0530

    removed unwanted router paths 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import ResponseInquiry from './pages/inquiry/responseinquiry';
 import { LoginBlade } from './components/dashboard/LoginBlade';
 import Master from './components/dashboard/dashboardlayouts/Master';
 
@@ -12,10 +11,8 @@ const App = () => {
             <BrowserRouter>
                 <Routes>
                     <Route path="/login" element={<LoginBlade />} />
-                    <Route path="/dashboard" element={<Master />} />
                     <Route path="/dashboard/*" element={<Master />} />
                     <Route path="/" element={<Navigate to="/login" />} />
-                    <Route path="/inquiry/response/:id" element={<ResponseInquiry />} />
                 </Routes>
             </BrowserRouter>
         </AuthProvider>
