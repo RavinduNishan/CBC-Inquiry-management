a848b1f9698437b5386950ca1f5e33f00e583c86
Ravindu Nishan Rathnayake <nish.ravindu@gmail.com>
2025-04-30 09:34:00 +0530

    Monodb connection error  resolved

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Export values from environment variables
-export const MONGODBURL = process.env.MONGODB_URL;
+export const MONGODBURL = process.env.MONGODB_URI;
 export const PORT = process.env.PORT || 5555;
 export const JWT_SECRET = process.env.JWT_SECRET || 'DefaultSecretForDevOnly-DoNotUseInProduction!123';
 
